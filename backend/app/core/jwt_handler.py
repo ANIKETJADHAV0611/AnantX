629d5ec6efb58866b691161c6e9134bb8964c43e
vibhorkdew <vibhorkdew@gmail.com>
2026-07-28 00:25:02 +0530

    Fix JWT environment variable names

--- backend/app/core/jwt_handler.py
+++ backend/app/core/jwt_handler.py
@@ -5,15 +5,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("JWT_SECRET")
-ALGORITHM = os.getenv("JWT_ALGORITHM")
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
 
 def create_access_token(data: dict):
-
     to_encode = data.copy()
 
     expire = datetime.utcnow() + timedelta(hours=2)
-
     to_encode.update({"exp": expire})
 
     return jwt.encode(
